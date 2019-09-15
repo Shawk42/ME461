5d1525dc2e1de5f70bc16235f85c69e287cbad23
Deryk <derahner@outlook.com>
2019-09-15 14:25:56 -0700

    Problem 1 Work in Progress
    Just made assumption regarding Sf
    - Sf = 0.5*Su if under 1400 MPa

--- Homework 8/Problem 1.py
+++ Homework 8/Problem 1.py
@@ -1,1 +1,33 @@
 import numpy as np
+from prettytable import PrettyTable
+
+""" Givens """
+Su = 500  # Mpa
+d = 5  # mm
+Nf = 10**6
+
+""" Table Values"""
+
+
+""" Intermediate Calculations """
+Sf_unmod = 0.5*Su
+
+""" Solution Calculations """
+
+
+""" Intermediate Outputs """
+inter = PrettyTable(["Variable", "Value", "Units"])
+inter.add_row(["Nf", Nf, "Cycles"])
+inter.add_row(["Sf (Unmodified)", Sf_unmod, "Mpa"])
+print(inter)
+print("")
+
+"""Solution"""
+sol = PrettyTable(["Variable", "Value", "Units"])
+sol.add_row(["Sf", "", ""])
+sol.add_row(["A", Su, "MPa"])
+sol.add_row(["B", "", ""])
+print(sol)
+print("")
+
+
