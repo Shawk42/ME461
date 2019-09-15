0948c2326622421c9d6319c2f418607da0f55738
Deryk <derahner@outlook.com>
2019-09-15 15:22:22 -0700

    Problem 1 Complete
    -Waiting for verification
    -Monte Carlo Simulation is the next task

--- Homework 8/Problem 1.py
+++ Homework 8/Problem 1.py
@@ -7,25 +7,44 @@ from prettytable import PrettyTable
 #d = 5  # mm
 
 # Material B
-Su = 100  # ksi
-d = 2  #in
+#Su = 100  # ksi
+#d = 2  #in
 
+# Material C
+#Su = 280  # ksi
+#d = 0.25  #in
+
+# Material D
+Su = 900  # MPa
+d = 10  #mm
 
 Nf = 10**6
 
 """ Table Values"""
-# Material A
+# Material A - axial
 #Ksize = 1
 #Kload = 0.8
 #Ksurf = 0.6
 
-# Material B
-Ksize = 0.8  # Max modification is 0.8 @ 50 mm (2 in = 50.8mm)
+# Material B - rotating bending
+#Ksize = 0.8  # Max modification is 0.8 @ 50 mm (2 in = 50.8mm)
+#Kload = 1
+#Ksurf = 0.9
+
+# Material C - axial
+#Ksize = 1
+#Kload = 0.8
+#Ksurf = 0.15
+
+# Material D - rotating bending
+Ksize = 1
 Kload = 1
 Ksurf = 0.9
 
 """ Intermediate Calculations """
 Sf_unmod = 0.5*Su
+#Sf_unmod = 100  # For Material C only due to Su > 200 ksi
+
 
 """ Solution Calculations """
 Sf = Ksize * Kload * Ksurf * Sf_unmod
@@ -40,6 +59,7 @@ inter = PrettyTable(["Variable", "Value", "Units"])
 inter.add_row(["Nf", Nf, "Cycles"])
 inter.add_row(["Sf (Unmodified)", Sf_unmod, "Mpa"])
 inter.add_row(["Sf (Verification)", Sf_check, ""])
+print("Intermediate Outputs")
 print(inter)
 print("")
 
@@ -48,13 +68,31 @@ sol = PrettyTable(["Material","Variable", "Value", "Units"])
 sol.add_row(["A", "Sf", 120, "MPa"])
 sol.add_row(["A","A", 500, "MPa"])
 sol.add_row(["A","B", -0.1, ""])
+
 sol.add_row(["B", "Sf", 36, "ksi"])
 sol.add_row(["B","A", 100, "ksi"])
 sol.add_row(["B","B", -0.07, ""])
-sol.add_row(["C", "Sf", Sf, ""])
-sol.add_row(["C","A", A, ""])
-sol.add_row(["C","B", B, ""])
+
+sol.add_row(["C", "Sf", 12, "ksi"])
+sol.add_row(["C","A", 280, "ksi"])
+sol.add_row(["C","B", -0.22, ""])
+
+sol.add_row(["D", "Sf", 405, "MPa"])
+sol.add_row(["D","A", 900, "MPa"])
+sol.add_row(["D","B", -0.05, ""])
+print("Solution Values")
 print(sol)
 print("")
+
+"""Live Linked Table"""
+livelink = PrettyTable(["Material","Variable", "Value", "Units"])
+livelink.add_row(["", "Sf", Sf, ""])
+livelink.add_row(["","A", A, ""])
+livelink.add_row(["","B", B, ""])
+print("Live Output")
+print(livelink)
+
+
+
 
 
