c3b9003dc7fdd9ff88f3d688097916d78e40f0d8
Deryk <derahner@outlook.com>
2019-09-15 16:08:02 -0700

    Problem 1 Complete
    -Awaiting Verfication
    -Monte Carlo Simulation run with comments

--- Homework 8/Problem 1.py
+++ Homework 8/Problem 1.py
@@ -1,10 +1,12 @@
 import numpy as np
+import numpy.random as rand
 from prettytable import PrettyTable
+import matplotlib.pyplot as plt
 
 """ Givens """
 # Material A
-#Su = 500  # Mpa
-#d = 5  # mm
+Su = 500  # Mpa
+d = 5  # mm
 
 # Material B
 #Su = 100  # ksi
@@ -15,16 +17,16 @@ from prettytable import PrettyTable
 #d = 0.25  #in
 
 # Material D
-Su = 900  # MPa
-d = 10  #mm
+#Su = 900  # MPa
+#d = 10  #mm
 
 Nf = 10**6
 
 """ Table Values"""
 # Material A - axial
-#Ksize = 1
-#Kload = 0.8
-#Ksurf = 0.6
+Ksize = 1
+Kload = 0.8
+Ksurf = 0.6
 
 # Material B - rotating bending
 #Ksize = 0.8  # Max modification is 0.8 @ 50 mm (2 in = 50.8mm)
@@ -37,14 +39,13 @@ Nf = 10**6
 #Ksurf = 0.15
 
 # Material D - rotating bending
-Ksize = 1
-Kload = 1
-Ksurf = 0.9
+#Ksize = 1
+#Kload = 1
+#Ksurf = 0.9
 
 """ Intermediate Calculations """
 Sf_unmod = 0.5*Su
 #Sf_unmod = 100  # For Material C only due to Su > 200 ksi
-
 
 """ Solution Calculations """
 Sf = Ksize * Kload * Ksurf * Sf_unmod
@@ -92,6 +93,46 @@ livelink.add_row(["","B", B, ""])
 print("Live Output")
 print(livelink)
 
+"""
+Monte Carlo Simulation
+
+I am curious into the results of varying the range of acceptable values in this equation.
+Material A will be used for these simulations
+
+Kload will vary uniformly between 0.7 and 0.8
+Ksurf will vary with a triangular distribution centered at 0.6, lower bound of 0.55, upper bound of 0.65
+"""
+
+Ksize = np.ones(250)
+Kload = rand.uniform(0.7,0.8,250)
+Ksurf = rand.triangular(0.55, 0.6, 0.65, 250)
+
+Sf = Ksize * Kload * Ksurf * Sf_unmod
+
+Sf_mean = round(np.mean(Sf), 2)
+ST_std = round(np.std(Sf), 2)
+
+print("")
+print("Monte Carlo Simulation Output")
+print("Mean", Sf_mean)
+print("Standard Deviation",ST_std)
+
+
+plt.hist(Sf, rwidth= 0.8, align= 'mid')
+plt.xlabel("Sf [MPa]")
+plt.ylabel("Count")
+plt.show()
+
+"""
+Results 
+
+I need more practice with Monte Carlo simulations outside of Excel. 
+The resulting distribution looks somewhat normal. The average of 112, and a standard 
+deviation of 5 means that the ordinal solution may be an overestimation of the life at 
+10^6 cycles. However, uniform and triangular distributions may not capture the true spread
+of the fundamental data.
+
+"""
 
 
 
