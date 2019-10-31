ce70de39a2298173ccd683c5b9466fcad71998e9
Deryk <derahner@outlook.com>
2019-10-31 11:13:45 -0700

    Homework 21 -Problem 6.1 --Unverified solutions --Only for a = 6 mm

--- Homework 21/Problem 6.1.py
+++ Homework 21/Problem 6.1.py
@@ -19,6 +19,7 @@ a = a * 0.001
 P = P * 1000
 
 """Tension"""
+print("Tension Component")
 aw = a / w
 print("A/W = ", aw)
 Y_ten = float(input("What is Y value for a = 6mm "))  # 2
@@ -26,8 +27,15 @@ qq = (P * a**(1/2))/(B * w)
 K_I_ten = Y_ten * qq
 
 """Bending"""
+print("Bending Component")
+# Assuming pure bending
+M = (60 * 0.001) * P
+Y_ben = float(input("What is the Y value for a = 6mm ")) # 1.9
+ee = (6 * M * a ** (1/2))/(B * w)**2
+K_I_ben = Y_ben * ee
 
-
+"""Superposition"""
+K_I = K_I_ben + K_I_ten
 
 """Table Output"""
 print("")
@@ -35,5 +43,9 @@ tab = PrettyTable()
 tab.field_names = ["Variable", "Value", "Units", "Value Type"]
 tab.add_row(["a/W", aw, "dim", "Intermediate"])
 tab.add_row(["K_I_ten", K_I_ten, "UNITS", "Intermediate" ])
+tab.add_row(["M", M, "N - m", "Intermediate"])
+tab.add_row(["K_I_ben", K_I_ben, "UNITS", "Intermediate"])
+tab.add_row(["K_I", K_I,"UNITS","SOLUTION"])
+
 
 print(tab)
