41c627cc69a52ae4effde3d7aeb764288b101fe9
Deryk <derahner@outlook.com>
2019-10-30 22:13:05 -0700

    Homework 21
    -Started Problem 21
    --Need assistance with superposition
    --Need assistance with mechanics

---
+++ Homework 21/Problem 6.1.py
@@ -0,0 +1,39 @@
+import numpy as np
+from prettytable import PrettyTable
+
+"""
+Prompt
+For the given geometry of an edge crack of length a (6mm or 30mm) find K_I
+"""
+
+"""GIVEN"""
+w = 100  # mm
+B = 10  # mm
+a = 6  # mm
+P = 100  # kN
+
+"""Unit Conversion [meters or Newtons]"""
+w = w * 0.001
+B = B * 0.001
+a = a * 0.001
+P = P * 1000
+
+"""Tension"""
+aw = a / w
+print("A/W = ", aw)
+Y_ten = float(input("What is Y value for a = 30mm "))  # 2
+qq = (P * a**(1/2))/(B * w)
+K_I_ten = Y_ten * qq
+
+"""Bending"""
+
+
+
+"""Table Output"""
+print("")
+tab = PrettyTable()
+tab.field_names = ["Variable", "Value", "Units", "Value Type"]
+tab.add_row(["a/W", aw, "dim", "Intermediate"])
+tab.add_row(["K_I_ten", K_I_ten, "UNITS", "Intermediate" ])
+
+print(tab)
